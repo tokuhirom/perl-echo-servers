8f6af5524950f48ff2fd17e2d0e0c2f761c91926
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-09-23 17:57:24 +0900

    added prefork server

---
+++ echo-prefork.pl
@@ -0,0 +1,46 @@
+use strict;
+use warnings;
+use Parallel::Prefork;
+use Getopt::Long;
+use IO::Socket::INET;
+use Parallel::Prefork;
+
+my $concurrent = 10;
+my $port = 9040;
+GetOptions(
+    'concurrent=s' => \$concurrent,
+    'port=i'       => \$port,
+);
+
+print "$0: http://localhost:$port/\n";
+print "concurrency: $concurrent\n";
+
+my $sock = IO::Socket::INET->new(
+    LocalHost => 'localhost',
+    LocalPort => $port,
+    Listen    => 10,
+    ReuseAddr => 1
+) or die $!;
+
+my $pm = Parallel::Prefork->new(
+    {
+        max_workers  => $concurrent,
+        trap_signals => {
+            TERM => 'TERM',
+            HUP  => 'TERM',
+        },
+    }
+);
+while ( $pm->signal_received ne 'TERM' ) {
+    $pm->start and next;
+    while (1) {
+        my $csock = $sock->accept;
+        while (my $line = <$csock>) {
+            print $csock $line;
+        }
+    }
+    $pm->finish;
+}
+$pm->wait_all_children;
+
+
